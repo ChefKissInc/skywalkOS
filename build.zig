3dc392ea77d271629ac8377edf3c1f07f7062166
Visual <30368284+VisualDevelopment@users.noreply.github.com>
2021-08-09 13:12:54 +0300

    Add final newlines

--- build.zig
+++ build.zig
@@ -3,9 +3,14 @@ const std = @import("std");
 const deps = @import("./deps.zig");
 
 pub fn build(b: *std.build.Builder) void {
-    const exe = b.addExecutable("Fuse", "src/main.zig");
-    exe.setTarget(b.standardTargetOptions(.{}));
+    const exe = b.addExecutable("Fuse.exec", "src/main.zig");
+    exe.setTarget(.{
+        .cpu_arch = std.Target.Cpu.Arch.x86_64,
+        .os_tag = std.Target.Os.Tag.linux,
+        .abi = std.Target.Abi.none,
+    });
     exe.setBuildMode(b.standardReleaseOptions());
+    exe.setOutputDir("../FWLauncher/Build/Drive");
     deps.addAllTo(exe);
     exe.install();
 }
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 // Copyright (c) 2021 VisualDevelopment. All rights reserved.
-const std = @import("std");
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+pub const start_sym_name = main;
+
+pub fn main() noreturn {
+    while (true) {}
 }
