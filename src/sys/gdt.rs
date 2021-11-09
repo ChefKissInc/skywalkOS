d5b38d27e33b98378aaf37db3707d67f4f26980c
Visual <30368284+VisualDevelopment@users.noreply.github.com>
2021-11-09 20:14:00 +0200

    Update code according to new AMD64 commit

--- src/sys/gdt.rs
+++ src/sys/gdt.rs
@@ -3,21 +3,21 @@
  * This project is licensed by the Creative Commons Attribution-NoCommercial-NoDerivatives licence.
  */
 
-pub static ENTRIES: [amd64::registers::gdt::SegmentDescriptor; 5] = [
-    amd64::registers::gdt::SegmentDescriptor::default(),
-    amd64::registers::gdt::SegmentDescriptor::new_from_ty(
-        amd64::registers::gdt::DescriptorType::CodeSegment,
+pub static ENTRIES: [amd64::sys::gdt::SegmentDescriptor; 5] = [
+    amd64::sys::gdt::SegmentDescriptor::default(),
+    amd64::sys::gdt::SegmentDescriptor::new_from_ty(
+        amd64::sys::gdt::DescriptorType::CodeSegment,
     ),
-    amd64::registers::gdt::SegmentDescriptor::new_from_ty(
-        amd64::registers::gdt::DescriptorType::DataSegment,
+    amd64::sys::gdt::SegmentDescriptor::new_from_ty(
+        amd64::sys::gdt::DescriptorType::DataSegment,
     ),
-    amd64::registers::gdt::SegmentDescriptor::new_from_ty(
-        amd64::registers::gdt::DescriptorType::TaskSegment,
+    amd64::sys::gdt::SegmentDescriptor::new_from_ty(
+        amd64::sys::gdt::DescriptorType::TaskSegment,
     ),
-    amd64::registers::gdt::SegmentDescriptor::default(),
+    amd64::sys::gdt::SegmentDescriptor::default(),
 ];
 
-pub static GDTR: amd64::registers::gdt::Gdtr = amd64::registers::gdt::Gdtr {
+pub static GDTR: amd64::sys::gdt::Gdtr = amd64::sys::gdt::Gdtr {
     limit: (core::mem::size_of_val(&ENTRIES) - 1) as u16,
     addr: ENTRIES.as_ptr(),
 };
