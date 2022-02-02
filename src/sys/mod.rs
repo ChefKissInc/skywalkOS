8e572c61753491124cead4174d4ee31c76405701
Visual <30368284+VisualDevelopment@users.noreply.github.com>
2022-02-02 18:03:41 +0200

    Fix copyright header dates

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) VisualDevelopment 2021-2021.
+ * Copyright (c) VisualDevelopment 2021-2022.
  * This project is licensed by the Creative Commons Attribution-NoCommercial-NoDerivatives licence.
  */
 
