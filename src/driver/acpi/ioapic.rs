981cc94fd2207bf5d85c276767d644eeaf993aad
Visual <30368284+ChefKissInc@users.noreply.github.com>
2022-07-24 00:38:19 +0300

    Correct license typo
    Signed-off-by: Visual <30368284+ChefKissInc@users.noreply.github.com>

--- src/driver/acpi/ioapic.rs
+++ src/driver/acpi/ioapic.rs
@@ -1,5 +1,5 @@
 //! Copyright (c) ChefKiss Inc 2021-2022.
-//! This project is licensed by the Creative Commons Attribution-NoCommercial-NoDerivatives licence.
+//! This project is licensed by the Creative Commons Attribution-NoCommercial-NoDerivatives license.
 
 use acpi::tables::madt::ic::ioapic::{self, IOAPICRedir};
 use amd64::spec::mps::{Polarity, TriggerMode};
