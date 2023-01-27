f6ad4a2d7ca34b55b0dea7c8deaf26a21cdec8f3
Visual <30368284+ChefKissInc@users.noreply.github.com>
2023-01-27 19:17:03 +0200

    Remove BGRT ACPI table
    Will not be used
    
    Signed-off-by: Visual <30368284+ChefKissInc@users.noreply.github.com>

--- Libraries/ACPI/src/tables/mod.rs
+++ Libraries/ACPI/src/tables/mod.rs
@@ -1,6 +1,5 @@
 // Copyright (c) ChefKiss Inc 2021-2023. Licensed under the Thou Shalt Not Profit License version 1.0. See LICENSE for details.
 
-pub mod bgrt;
 pub mod hpet;
 pub mod madt;
 pub mod mcfg;
