813783477d2885c5f0f876e2e80495f72c15670b
Visual <30368284+ChefKissInc@users.noreply.github.com>
2023-02-22 19:09:30 +0200

    device_tree.rs: Move OSDTEntryReqType to use statement
    Signed-off-by: Visual <30368284+ChefKissInc@users.noreply.github.com>

--- Kernel/src/system/proc/userland/handlers/device_tree.rs
+++ Kernel/src/system/proc/userland/handlers/device_tree.rs
@@ -1,25 +1,23 @@
 // Copyright (c) ChefKiss Inc 2021-2023. Licensed under the Thou Shalt Not Profit License version 1.0. See LICENSE for details.
 
+use tungstenkit::syscall::OSDTEntryReqType;
+
 use crate::system::{proc::scheduler::Scheduler, RegisterState};
 
 pub fn get_entry_info(scheduler: &mut Scheduler, state: &mut RegisterState) {
     let sys_state = unsafe { &mut *crate::system::state::SYS_STATE.get() };
     let dt_index = sys_state.dt_index.as_ref().unwrap().read();
-    let Ok(info_type) = tungstenkit::syscall::OSDTEntryReqType::try_from(state.rdx) else {
+    let Ok(info_type) = OSDTEntryReqType::try_from(state.rdx) else {
         todo!()
     };
     let Some(ent) = dt_index.get(&state.rsi) else {
         todo!()
     };
     let data = match info_type {
-        tungstenkit::syscall::OSDTEntryReqType::Parent => postcard::to_allocvec(&ent.lock().parent),
-        tungstenkit::syscall::OSDTEntryReqType::Children => {
-            postcard::to_allocvec(&ent.lock().children)
-        }
-        tungstenkit::syscall::OSDTEntryReqType::Properties => {
-            postcard::to_allocvec(&ent.lock().properties)
-        }
-        tungstenkit::syscall::OSDTEntryReqType::Property => {
+        OSDTEntryReqType::Parent => postcard::to_allocvec(&ent.lock().parent),
+        OSDTEntryReqType::Children => postcard::to_allocvec(&ent.lock().children),
+        OSDTEntryReqType::Properties => postcard::to_allocvec(&ent.lock().properties),
+        OSDTEntryReqType::Property => {
             let Ok(k) = core::str::from_utf8(unsafe {
                 core::slice::from_raw_parts(state.rcx as *const _, state.r8 as _)
             }) else {
