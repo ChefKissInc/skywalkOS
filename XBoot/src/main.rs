ec6d468af9ed873d0f93d35837073fbabad1a11a
Visual <30368284+ChefKissInc@users.noreply.github.com>
2023-04-04 21:29:42 +0300

    Bump deps
    Signed-off-by: Visual <30368284+ChefKissInc@users.noreply.github.com>

--- XBoot/src/main.rs
+++ XBoot/src/main.rs
@@ -3,6 +3,7 @@
 #![no_std]
 #![no_main]
 #![deny(warnings, clippy::cargo, clippy::nursery, unused_extern_crates)]
+#![allow(clippy::multiple_crate_versions)]
 #![feature(asm_const, core_intrinsics)]
 
 #[macro_use]
