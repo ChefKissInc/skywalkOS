1ef237997c0b773464ed84af058344bf7917396a
Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>
2025-05-10 01:04:55 +0200

    feat(SkyKit): handle double panic

--- Libraries/SkyKit/src/userspace/panic.rs
+++ Libraries/SkyKit/src/userspace/panic.rs
@@ -1,9 +1,19 @@
 // Copyright (c) ChefKiss 2021-2025. Licensed under the Thou Shalt Not Profit License version 1.5. See LICENSE for details.
 
-use crate::syscall::SystemCall;
+use crate::{syscall::SystemCall, userspace::logger::KWriter};
+use core::{
+    fmt::Write,
+    sync::atomic::{AtomicBool, Ordering},
+};
+
+static IN_PANIC: AtomicBool = AtomicBool::new(false);
 
 #[panic_handler]
 fn panic_handler(info: &core::panic::PanicInfo) -> ! {
-    error!("{info}");
+    if IN_PANIC.swap(true, Ordering::AcqRel) {
+        let _ = writeln!(KWriter, "double panic");
+    } else {
+        writeln!(KWriter, "{info}").unwrap();
+    }
     unsafe { SystemCall::quit() }
 }
